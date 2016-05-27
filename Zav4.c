94d8be9f040f3728e67a8e79e9b5dd2ea504bc09
Vlad667 <genjas213@gmail.com>
2016-05-27 08:36:21 +0300

    Create Zav4.c

---
+++ Zav4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main ()
+{
+    int max;
+    double i, sum = 0;
+	
+    printf("maxim(i)= ");
+    scanf("%d", &max);
+	
+    for (i = 1; i <= max; i++) {
+    	sum = 1 / pow ((2 * i), 2);
+    }
+    printf("\nSumma rivna %lf", sum);
+    system("pause")
+    return 0;
+}
