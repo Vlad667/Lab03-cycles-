fe2675167c164771c0b8fef4893fe221fe6af0ee
Vlad667 <genjas213@gmail.com>
2016-05-27 08:45:13 +0300

    Update Zav4(3).c

--- Zav4(3).c
+++ Zav4(3).c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int main () {
+
+    int max;
+    double k, sum = 0;
+      printf("max(k)=");
+    scanf("%d", &max);
+    
+    for (k = 1; k <= max; k++) {
+    	sum = 1 / pow ( k, 5);
+    }
+    printf("\nSumma rivna = %lf", sum);
+    system("pause");
+    return 0;
