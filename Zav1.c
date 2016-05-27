a5c820a7ef3c42966800347ac7ec2654ca558c70
Vlad667 <genjas213@gmail.com>
2016-05-27 08:34:47 +0300

    Update Zav1.c

--- Zav1.c
+++ Zav1.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <conio.h> 
+#include <stdlib.h>
 
+
+int main(void)
+{
+     printf("Vvedite chiclo \nn=");
+    int x, y, z;
+   
+    scanf("%d", &x);
+    for(y=2;y<x;y++)
+    {
+        int f = 0;
+         for (z=2;z<y;z++) {
+             if(y%z==0) {
+                 f=1;
+                 break;
+             }
+         }
+         if (f==0)
+             printf("%d\n",y);
+    }
+    system("pause");        
+    
+    return (0);   
+}
